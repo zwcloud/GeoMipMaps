238fcecd7638119d99b9c081688b0c5c6f88887c
Zou Wei <zwcloud@hotmail.com>
2020-10-04 15:25:33 +0800

    Implement block mesh generation for LODs
    A block contains 16x16 quads (17x17 vertices).

--- Assets/Landscape.cs
+++ Assets/Landscape.cs
@@ -1,24 +1,28 @@
-﻿using System.Collections.Generic;
+﻿//#define ONLY_LOD0
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
 public class Landscape
 {
-    public List<Mesh> LODMeshes = new List<Mesh>();
+    public List<List<Mesh>> LODMeshList = new List<List<Mesh>>();
 
     /// <summary>
     /// 
     /// </summary>
     /// <param name="heightMap">height map</param>
     /// <param name="size">size in world unit</param>
-    private static Landscape Create(Texture2D heightMap, Vector2 size)
+    /// <param name="baseHeight"></param>
+    /// <param name="totalHeight"></param>
+    private static Landscape Create(Texture2D heightMap, Vector2 size, float baseHeight,
+        float totalHeight)
     {
         if (heightMap == null)
         {
             throw new System.ArgumentNullException(nameof(heightMap));
         }
 
-        Landscape Landscape = new Landscape();
+        Landscape landscape = new Landscape();
         var resolution = new Vector2Int(heightMap.width, heightMap.height);
 
         if (resolution.x > 4097 || resolution.y > 4097 || resolution.x < 33 || resolution.y < 33)
@@ -34,97 +38,142 @@ public class Landscape
         }
         
         var quadNumber = resolution - Vector2Int.one;
-
+        const int blockQuadNumber = 16;
+        int LOD = 0;
         while (quadNumber.x > 16 && quadNumber.y > 16)
         {
-            var vertexNumber = quadNumber + Vector2Int.one;
-            var quadSize = new Vector2(size.x / quadNumber.x, size.y / quadNumber.y);
-            
-            var positions = new Vector3[vertexNumber.x * vertexNumber.y];
-            var texCoords = new Vector2[vertexNumber.x * vertexNumber.y];
-            var indexes = new int[quadNumber.x * quadNumber.y * 6];
-            var index = 0;
-
-            unsafe
+            var blockNumber = quadNumber / blockQuadNumber;
+            var blockMeshes = new List<Mesh>(blockNumber.x * blockNumber.y);
+            var blockPixelSize =
+                new Vector2Int(resolution.x / blockNumber.x, resolution.y / blockNumber.y);
+            var quadSize = size / resolution;
+            var blockPixelStep = new Vector2Int(
+                (resolution.x - 1)/ quadNumber.x,
+                (resolution.y - 1)/ quadNumber.y);
+            for (int blockY = 0; blockY < blockNumber.y; blockY++)
             {
-                var w = vertexNumber.x;
-                var h = vertexNumber.y;
-                fixed (Vector3* positions_ptr = positions)
-                fixed (Vector2* texCoords_ptr = texCoords)
+                for (int blockX = 0; blockX < blockNumber.x; blockX++)
                 {
-                    for (var y = 0; y < h; y++)
-                    {
-                        for (var x = 0; x < w; x++)
-                        {
-                            var u = 1.0f * x / w;
-                            var v = 1.0f * y / h;
-                            //R channel is used as the height
-                            var height = heightMap.GetPixelBilinear(u, v).r;
-                            positions_ptr[y * w + x]
-                                = new Vector3(x * quadSize.x, height, y * quadSize.y);
-                            texCoords_ptr[y * w + x]
-                                = new Vector2((float) x / (w - 1), (float) y / (h - 1));
-                        }
-                    }
-                }
+                    var blockPixelMin = new Vector2Int(blockX, blockY) * blockPixelSize;
+                    var blockPixelMax = blockPixelMin + Vector2Int.one * blockPixelSize;
 
-                fixed (int* indexes_ptr = indexes)
-                {
-                    for (var y = 0; y < h - 1; y++)
-                    {
-                        for (var x = 0; x < w - 1; x++)
-                        {
-                            //different split direction of a cell
-#if f
-                            indexes_ptr[index++] = (y*w) + x;
-                            indexes_ptr[index++] = ((y+1) * w) + x;
-                            indexes_ptr[index++] = (y*w) + x + 1;
+                    Debug.Log($"LOD{LOD} Block<{blockX},{blockY}>:" +
+                              $" from {blockPixelMin} to {blockPixelMax} step {blockPixelStep}" +
+                              " on the height map");
 
-                            indexes_ptr[index++] = ((y+1) * w) + x;
-                            indexes_ptr[index++] = ((y+1) * w) + x + 1;
-                            indexes_ptr[index++] = (y*w) + x + 1;
-#else
-                            indexes_ptr[index++] = (y * w) + x;
-                            indexes_ptr[index++] = ((y + 1) * w) + x;
-                            indexes_ptr[index++] = ((y + 1) * w) + x + 1;
-
-                            indexes_ptr[index++] = (y * w) + x;
-                            indexes_ptr[index++] = ((y + 1) * w) + x + 1;
-                            indexes_ptr[index++] = (y * w) + x + 1;
-#endif
-                        }
-                    }
+                    var mesh = CreateBlockMesh(
+                        blockPixelMin, blockPixelMax, blockPixelStep,
+                        baseHeight, totalHeight, quadSize, heightMap);
+                    mesh.name = $"Block<{blockX},{blockY}>";
+                    blockMeshes.Add(mesh);
                 }
             }
 
-            Mesh mesh = new Mesh();
-            mesh.vertices = positions;
-            mesh.uv = texCoords;
-            mesh.triangles = indexes;
-            mesh.RecalculateNormals();
-            Landscape.LODMeshes.Add(mesh);
-
+            landscape.LODMeshList.Add(blockMeshes);
+            LOD++;
+#if ONLY_LOD0
+            break;
+#endif
             quadNumber /= 2;
         }
 
-        return Landscape;
+        return landscape;
+    }
+    
+    private static Mesh CreateBlockMesh(
+        Vector2Int pixelMin, Vector2Int pixelMax,
+        Vector2Int pixelStep,
+        float baseHeight, float totalHeight, Vector2 quadSize, Texture2D heightMap)
+    {
+        var pixelNumber = pixelMax - pixelMin + Vector2Int.one;
+
+        var vertexNumber
+            = new Vector2Int(
+                (pixelNumber.x - 1) / pixelStep.x + 1, (pixelNumber.y - 1) / pixelStep.y + 1);
+        var positions = new Vector3[vertexNumber.x * vertexNumber.y];
+        var texCoords = new Vector2[vertexNumber.x * vertexNumber.y];
+        var indexes = new int[(vertexNumber.x - 1) * (vertexNumber.y - 1) * 6];
+
+        var w = vertexNumber.x;
+        var h = vertexNumber.y;
+        int i = 0;
+        for (var y = pixelMin.y; y <= pixelMax.y; y+=pixelStep.y)
+        {
+            for (var x = pixelMin.x; x <= pixelMax.x; x+=pixelStep.x)
+            {
+                //R channel is used as the height
+                var normalizedHeight = heightMap.GetPixel(x, y).r;
+                var height = baseHeight + normalizedHeight * totalHeight;
+                positions[i]
+                    = new Vector3(x * quadSize.x, height, y * quadSize.y);
+
+                //TODO consider texCoords
+                texCoords[i]
+                    = new Vector2((float) x / (w - 1), (float) y / (h - 1));
+                //TODO consider normal, especially at edges neighbor to another block
+                i++;
+            }
+        }
+
+        var index = 0;
+        for (var y = 0; y < h - 1; y++)
+        {
+            for (var x = 0; x < w - 1; x++)
+            {
+                //different split direction of a cell
+#if f
+                indexes[index++] = (y*w) + x;
+                indexes[index++] = ((y+1) * w) + x;
+                indexes[index++] = (y*w) + x + 1;
+
+                indexes[index++] = ((y+1) * w) + x;
+                indexes[index++] = ((y+1) * w) + x + 1;
+                indexes[index++] = (y*w) + x + 1;
+#else
+                indexes[index++] = (y * w) + x;
+                indexes[index++] = ((y + 1) * w) + x;
+                indexes[index++] = ((y + 1) * w) + x + 1;
+
+                indexes[index++] = (y * w) + x;
+                indexes[index++] = ((y + 1) * w) + x + 1;
+                indexes[index++] = (y * w) + x + 1;
+#endif
+            }
+        }
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = positions;
+        mesh.uv = texCoords;
+        mesh.triangles = indexes;
+        mesh.RecalculateNormals();
+        return mesh;
     }
 
     [MenuItem("GeoMipMaps/Create Test Landscape Objects")]
     public static void CreateTestLandscapeObjects()
     {
         var heightMap = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/HeightMap.png");
-        var landscape = Create(heightMap, new Vector2(500, 500));
-        var meshes = landscape.LODMeshes;
+        var landscape = Create(heightMap, new Vector2(1000, 1000), 10, 50);
         var material = AssetDatabase.GetBuiltinExtraResource<Material>("Default-Diffuse.mat");
-        for (var LOD = 0; LOD < meshes.Count; LOD++)
+
+        var LODCount = landscape.LODMeshList.Count;
+        for (var LOD = 0; LOD < LODCount; LOD++)
         {
-            var mesh = meshes[LOD];
-            GameObject obj = new GameObject($"Landscape_LOD{LOD}");
-            obj.AddComponent<MeshFilter>().sharedMesh = mesh;
-            var renderer = obj.AddComponent<MeshRenderer>();
-            renderer.material = Object.Instantiate(material);
-            renderer.sharedMaterial.color = new Color(0.2f, 0.2f, 1.0f - LOD * 0.8f / meshes.Count);
+            var blockMeshList = landscape.LODMeshList[LOD];
+            GameObject LODObj = new GameObject($"Landscape_LOD{LOD}");
+            LODObj.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+            var LODColor = new Color(0.2f, 0.2f, 1.0f - LOD * 0.8f / LODCount);
+            for (int i = 0; i < blockMeshList.Count; i++)
+            {
+                var mesh = blockMeshList[i];
+                var blockObj = new GameObject(mesh.name);
+                blockObj.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+                blockObj.AddComponent<MeshFilter>().sharedMesh = mesh;
+                var renderer = blockObj.AddComponent<MeshRenderer>();
+                renderer.material = Object.Instantiate(material);
+                renderer.sharedMaterial.color = LODColor;
+                blockObj.transform.SetParent(LODObj.transform, true);
+            }
         }
     }
 }
